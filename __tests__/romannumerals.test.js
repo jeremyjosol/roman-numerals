1044b0ead187803bc1c5a4d9659250cc3c50c510
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-06 15:40:38 -0800

    Pass test for simplest case

--- __tests__/romannumerals.test.js
+++ __tests__/romannumerals.test.js
@@ -1,4 +1,8 @@
 import { intToRomanNumeral } from './../src/js/romannumerals.js';
 
-// describe('Constructor', () => {}
-// test() => {}
+describe('intToRomanNumeral', () => {
+  test('it should return a message if int is invalid', () => {
+    const userInput = intToRomanNumeral("!");
+    expect(userInput).toEqual("Valid Roman numerals are between a minimum value of 1 and maximum value of 3999.");
+  });
+});
--- src/js/romannumerals.js
+++ src/js/romannumerals.js
@@ -1,3 +1,4 @@
-export function intToRomanNumerals(int) {
-  
+export function intToRomanNumeral(int) {
+  if (isNaN(int) || int < 1 || int > 3999 )
+  return "Valid Roman numerals are between a minimum value of 1 and maximum value of 3999."
 }
